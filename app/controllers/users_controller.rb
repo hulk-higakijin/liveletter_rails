92bcd03e0de024d6a3fdd881875f849dcb3cdba5
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-03 15:49:19 +0900

    feat: post status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @posts = Post.where(user_id: @user.id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :emoji, presence: true, length: { is: 1 }
 
-  enum status: { initial: 0, draft: 1, published: 2, archived: 3 }
+  enum :status, { initial: 0, draft: 1, published: 2, archived: 3 }
 end
